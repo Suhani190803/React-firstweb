db21a2964c2aeb0092a9f7b21fb5974e16db5c1b
Suhani Mishra <suhanimishra198@gmail.com>
2025-12-12 19:33:09 +0530

    New React App

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Suhani Mishra";
+  let l =[10,20,30,40];
+  let obj={
+    name : "sanya",
+    age : 34 ,
+    city : "Bihar",
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{name}</h1>
+      <h2>{l.map((v)=>{
+        return(
+          <div>{v}</div>
+        )
+      })}</h2>
+
+      <div>{obj.city}</div>
+      <h3>{10+30}</h3>
     </div>
   );
 }
