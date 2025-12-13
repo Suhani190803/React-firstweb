0958524c7be0cbe82445720f1f6b0739da05cf96
Suhani Mishra <suhanimishra198@gmail.com>
2025-12-13 11:36:10 +0530

    Import & Export Component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
-  const name = "Suhani Mishra";
-  let l =[10,20,30,40];
-  let obj={
-    name : "sanya",
-    age : 34 ,
-    city : "Bihar",
-  }
-  const status = true;
+  
   return (
     <div className="App">
-      <h1>{name}</h1>
-      <h2>{l.map((v)=>{
-        return(
-          <div>{v}</div>
-        )
-      })}</h2>
-
-      <div>{obj.city}</div>
-      <h3>{10+30}</h3>
-      {(status) ? <h4 style ={{color : "red",  backgroundColor :"yellow"}}>Welcome back guyss</h4> : <h4>Not welcoming guyss</h4>}
-    
-    </div>
+        <h2>Hello</h2>
+         <Header/>
+         <Footer/>
+         <div className='row'>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+         </div>
+         
+      </div>
   );
 }
 
 export default App;
+
+
+function Card(){
+  return(
+    <div className='cardItems'>Card Div</div>
+  )
+}
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    return(
+        <div>
+            <h1>welcome to Header Section</h1>
+            <h2>Welcome to hell guys....!!!!!!</h2>
+        </div>
+    )
+}
+
+export default Header;
