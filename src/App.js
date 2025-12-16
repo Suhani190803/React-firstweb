c1872efa963367629154c0f981c2ed464f9eb202
Suhani Mishra <suhanimishra198@gmail.com>
2025-12-16 16:18:43 +0530

    Hide and show password

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 
 import { useState } from 'react';
 import './App.css';
-import BtnModule from './Button.module.css'
+import BtnModule from './Button.module.css' 
 
 
 
 
 
 function App() {
+   const[pstatus,setPstatus]=useState(false)
+   
   const[status,setStatus]=useState(false);
   return (
     <div className="App">
+     <input type={pstatus ? 'text' : 'password'}/>   
+      <button onClick={()=>setPstatus(!pstatus)}>{pstatus ? 'Hide' : 'show'}</button>
 
-      <button className={BtnModule.error}>Error</button>
+
+
+
+      {/* <button className={BtnModule.error}>Error</button> */}
+      <br/> 
+      {/* <button className={BtnModule.warning}>Demo</button> */}
       <br/>
-      <button className={BtnModule.warning}>Demo</button>
-      <br/>
-      <button onClick={()=>setStatus(!status)}>{(status)? 'Hide' : 'Show'}</button>
-      {
+      {/* <button onClick={()=>setStatus(!status)}>{(status)? 'Hide' : 'Show'}</button> */}
+      {/* {
       (status)
       ?
       <p className='bg-pink-400'>Welcome to India guyss...</p>
       :
       ''
        }
-    
+     */}
      
      
     </div>
