a4c15666453cf30eed087ee577be9e6c9e2f9dbc
Suhani Mishra <suhanimishra198@gmail.com>
2025-12-16 09:13:42 +0530

    If-else statement in React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import { useState } from 'react';
 
 
 function App() {
+  let template ='';
+  const[count,setCount]=useState(1)
+  const[pshow,setPshow]=useState(true)
+  if(pshow){
+    template=<Card/>
+  //  template=<>
+   {/* <button className='bg-pink-800 p-4' onClick={()=>setPshow(!pshow)}>Hide</button>
+   <p>Welcome to India</p>
+   </> */}
+  }
+  else{
+    // template=<button className='bg-blue-900 p-4' onClick={()=>setPshow(!pshow)}>Show</button>
+    template=''
+  }
+
   let displayData =()=>{
-    alert("Welcome to you")
+    setCount(count+1);
   }
   const addData=(a,b)=>{
     console.log(a+b);
@@ -15,13 +31,25 @@ function App() {
     <div className="App">
 
      {/* <Header/> */}
-     <button className='bg-[red] p-[10px] mr-4' onClick={()=>addData(10,45)} >Add Data</button>
-     <button className='bg-[red] p-[10px]' onClick={displayData} >Save</button>
-    
+     {/* <h2>Count: {count}</h2> */}
+     {/* <button className='bg-[red] p-[10px] mr-4' onClick={()=>addData(10,45)} >Add Data</button> */}
+     {/* <button className='bg-[red] p-[10px]' onClick={displayData} >Save</button> */}
+     <br/>
+     {/* <button >Show</button> */}
+     <p>{template}</p>
+     
+     
     </div>
   );
 }
 
 export default App;
+
+
+const Card =()=>{
+  return(
+    <h1>Welcome to my paragraph</h1>
+  )
+}
 
 
