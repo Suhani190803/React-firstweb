f61c67401dfe7193063eb40b29a2ae0ef587b9da
Suhani Mishra <suhanimishra198@gmail.com>
2025-12-16 16:47:50 +0530

    Responsive menu

--- src/App.js
+++ src/App.js
@@ -11,8 +11,23 @@ function App() {
    const[pstatus,setPstatus]=useState(false)
    
   const[status,setStatus]=useState(false);
+  const[menustatus,setMenustatus]=useState(false);
   return (
     <div className="App">
+      <button className='micon' onClick={()=>setMenustatus(!menustatus)}>
+        {menustatus ? <span>&times;</span> : <span>&#9776;</span>}
+        
+        </button>
+      <div className={`menu ${menustatus ? 'activeMenu' : ''}`}>
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Menu details</li>
+          <li>Photos</li>
+          <li>Contact us</li>
+        </ul>
+
+      </div>
      <input type={pstatus ? 'text' : 'password'}/>   
       <button onClick={()=>setPstatus(!pstatus)}>{pstatus ? 'Hide' : 'show'}</button>
 
