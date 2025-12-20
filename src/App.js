55b8970762d40e0a5b081dfa59636cecd53b7d3e
Suhani Mishra <suhanimishra198@gmail.com>
2025-12-20 19:59:19 +0530

    Event Handling

--- src/App.js
+++ src/App.js
@@ -1,151 +1,64 @@
 
+
 import { useState } from 'react';
 import './App.css';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-import BtnModule from './Button.module.css' 
-import question from './faqQuestion';
-import Faqs from './Faqs';
-
-
-
 
 
 function App() {
-  //  const[pstatus,setPstatus]=useState(false)
+  // const[name,setName] = useState("");
+  // const[password,setPswd]=useState("")
+
+  const [formData , setFormData] = useState(
+    {
+     uname : '',
+     upswd : '',
+     uemail : '',
+     ucontact : '',
+     index : ''
+  }
+)
+  const[userData,setUserData]=useState([])
    
-  // const[status,setStatus]=useState(false);
-  // const[menustatus,setMenustatus]=useState(false);
-  
-  // const[modelstatus,setModalstatus]=useState(false);
-  //  let n=["Ram","Shyam","Sundar" , "Shivam"]
-     let showNotification = () => {
-        // toast.info("Welcome back Suhani");
-        // toast.success("Data saved successfully");
-        // toast.error("Something went wrong");
-        toast.warning("Please fill all fields");
 
 
-    };
 
 
+  const getValue=(event)=>{
+     let oldData ={...formData}
+     let inputName=event.target.name;
+     let inputValue=event.target.value;
+     oldData[inputName]=inputValue;
+     setFormData(oldData)
+     
+  }
+   
+  let handleSubmit=(event)=>{
+    event.preventDefault()
+  }
+    
   return (
-    <div className="App">
-      <button onClick={showNotification}>Save</button>
-      <ToastContainer />
-
-      
-
-
-
-
-       {/* <ul>
-           {n.map((items,id)=>{
-            return(
-              <li key ={id}>{items}</li>
-            )
-           })}
-       </ul> */}
-
-
-
-
-
-
-      {/* <Faqs/> */}
-    {/* //   <div>
-    //     <h1>Frequently Asked Questions (FAQs)</h1>
-    //     <div className='faqouter'> */}
-         {/* {question.map((faqItems,i)=>{
-          return(
-            <div className='faqItems'>
-            <h2>What is Web Development?</h2>
-            <p>Web development is the process 
-              of creating websites and web applications using technologies like HTML, CSS, JavaScript, 
-              and frameworks like React.
-            </p>
-          </div>
-          )
-          })} */}
-
-
-          
-      {/* //     <div className='faqItems'>
-      //       <h2>What is Web Development?</h2>
-      //       <p>Web development is the process 
-      //         of creating websites and web applications using technologies like HTML, CSS, JavaScript, 
-      //         and frameworks like React.
-      //       </p>
-      //     </div>
-      //     <div className='faqItems'>
-      //       <h2>What is Web Development?</h2>
-      //       <p>Web development is the process 
-      //         of creating websites and web applications using technologies like HTML, CSS, JavaScript, 
-      //         and frameworks like React.
-      //       </p>
-      //     </div>
-      //     <div className='faqItems'>
-      //       <h2>What is Web Development?</h2>
-      //       <p>Web development is the process 
-      //         of creating websites and web applications using technologies like HTML, CSS, JavaScript, 
-      //         and frameworks like React.
-      //       </p>
-      //     </div>
-      //   </div> */}
-      {/* // </div> */}
-
-
-
-
-
-      {/* <button  className='en' onClick={()=>setModalstatus(true)}>Enquire Now</button>
-       <div onClick={()=>setModalstatus(false)} className={`modalOverlay ${modelstatus ? 'modalShow': '' }`}></div>
-       <div className={`ModalDiv ${modelstatus ? 'showModalDiv': '' }`}>
-        <h3>Enquiry Now <span onClick={()=>setModalstatus(false)}>&times;</span></h3>
-       </div>
-
-
-      <button className='micon' onClick={()=>setMenustatus(!menustatus)}>
-        {menustatus ? <span>&times;</span> : <span>&#9776;</span>}
-        
-        </button>
-      <div className={`menu ${menustatus ? 'activeMenu' : ''}`}>
-        <ul>
-          <li>Home</li>
-          <li>About us</li>
-          <li>Menu details</li>
-          <li>Photos</li>
-          <li>Contact us</li>
-        </ul>
-
-      </div>
-     <input type={pstatus ? 'text' : 'password'}/>   
-      <button onClick={()=>setPstatus(!pstatus)}>{pstatus ? 'Hide' : 'show'}</button> */}
-
-
-
-
-      {/* <button className={BtnModule.error}>Error</button> */}
-      <br/> 
-      {/* <button className={BtnModule.warning}>Demo</button> */}
-      <br/>
-      {/* <button onClick={()=>setStatus(!status)}>{(status)? 'Hide' : 'Show'}</button> */}
-      {/* {
-      (status)
-      ?
-      <p className='bg-pink-400'>Welcome to India guyss...</p>
-      :
-      ''
-       }
-     */}
-     
-     
+    <div>
+      <form className='App' onSubmit={handleSubmit}>
+        <h1>Enquiry Now</h1>
+        <label>First Name :</label><br/> 
+        <input type= "text" onChange={getValue} name='uname' value={formData.uname}  /><br/>
+        <label>Password :</label><br/> 
+        <input type= "text" onChange={getValue} name='upswd' value={formData.upswd} /><br/>
+        <label>Email :</label><br/> 
+        <input type= "text"  onChange={getValue} name='uemail' value={formData.uemail} /><br/>
+        <label>Contact :</label><br/> 
+        <input type= "text" onChange={getValue}  name='ucontact' value={formData.ucontact} />
+         
+      </form>
+     <button> 
+      {
+      formData.index !== "" ? 'Update' : 'Save'
+      }
+      </button>
     </div>
-  );
+    
+  )
 }
 
 export default App;
-
-
 
